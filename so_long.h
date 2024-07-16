fca902b6302ac496b7771c633fb8407bebfa88c5
Ndonda Daniel Matondo 42 <nmatondo@c3r5s7.42luanda.com>
2024-07-16 15:53:44 +0100

    add files of project so long

---
+++ so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmatondo <nmatondo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:17:04 by nmatondo          #+#    #+#             */
+/*   Updated: 2024/07/16 14:33:23 by nmatondo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+# include "minilibx-linux/mlx.h"
+# include "Libft/libft.h"
+# include <fcntl.h>
+
+int	close_window(void *param);
+int	map_validator(char *file_name);
+
+#endif
