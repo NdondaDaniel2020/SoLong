74ca67701db00e35da5290b2666a784e7d6d8e6e
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2024-07-31 13:33:40 +0100

    Validação
    A base da validação do mapa do jogo e formato de arquivo foi criada\nbased on game map validation and file format

--- so_long.c
+++ so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmatondo <nmatondo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 14:23:41 by nmatondo          #+#    #+#             */
-/*   Updated: 2024/07/19 11:59:45 by nmatondo         ###   ########.fr       */
+/*   Updated: 2024/07/31 12:14:30 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	main(int ac, char **av)
 {
 	char	*file;
-
+	
+	(void)ac;
 	file = open_file(av[1]);
 	ft_printf("%s\n", file);
 	free(file);
