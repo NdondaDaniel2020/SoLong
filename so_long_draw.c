3613c1196b39a9906b95d74234cab2b841f719c8
Ndonda Daniel Matondo vir-kali (nmatondo) <kali@kali>
2024-08-07 13:52:33 +0100

    Draw portal

--- so_long_draw.c
+++ so_long_draw.c
@@ -78,10 +78,30 @@ void	draw_player(t_wind *win, int l, int y)
 	}
 }
 
+static int	pos_portal(t_wind *win)
+{
+	int		r;
+	int		l;
+	t_point	pos;
+
+	r = 1;
+	l = 1;
+	pos = find_in_matrix(win->map_matrix, 'E');
+	while (win->map_matrix[pos.y][pos.x + r] == '0')
+		r++;
+	while (win->map_matrix[pos.y][pos.x - l] == '0')
+		l++;
+	if (l > r)
+		return (1);
+	else
+		return (0);
+}
+
 static void	draw_portal(t_wind *win, int l, int y)
 {
 	int		c;
 	int		x;
+	int		pos;
 	void	*img;
 
 	c = 0;
@@ -90,7 +110,8 @@ static void	draw_portal(t_wind *win, int l, int y)
 	{
 		if (win->map_matrix[l][c] == 'C')
 		{
-			img = win->star.img_ptr;
+			pos = pos_portal(win);
+			img = win->ptl[pos].img_ptr;
 			mlx_put_image_to_window(win->mlx, win->win, img, x, y);
 		}
 		x += 50;
