93285d89a7e9ba2acd6ed01040c55978fd7f9bae
Ndonda Daniel Matondo vir-kali (nmatondo) <kali@kali>
2024-08-16 08:58:21 +0100

    Norma

--- so_long_windows.c
+++ so_long_windows.c
@@ -34,12 +34,6 @@ int	key_press(int keycode, t_wind *win)
 		list = ft_lstnew((void *)char_lst('w'));
 		ft_lstadd_back(&win->move, list);
 	}
-	if (keycode == 113)
-	{
-		list = ft_lstnew((void *)char_lst('q'));
-		ft_lstadd_back(&win->move, list);
-	}
-	ft_printf("%i\n", keycode);
 	key_pess_enemy(keycode, win);
 	return (0);
 }
