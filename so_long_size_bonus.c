8e84512fc4866f1dd87c0671b98bc5899ecbb941
Ndonda Daniel Matondo vir-kali (nmatondo) <kali@kali>
2024-08-23 23:08:03 +0100

    ajusta

--- so_long_size_bonus.c
+++ so_long_size_bonus.c
@@ -89,7 +89,7 @@ t_size	size_map(char *map)
 	size.w = width(map);
 	size.h = height(map);
 	oco = count_occurrence_str(map, '\n');
-	if (oco >= size.h)
+	if (oco != size.h)
 		size.h = 0;
 	return (size);
 }
